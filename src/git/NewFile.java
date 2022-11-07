9b85188bb29b990c5c03f58a33f0dc7b2a0e655d
seungjae-roh <gkavudshTl@gmail.com>
2022-11-07 12:44:31 +0900

    두번째파일 생

--- src/git/NewFile.java
+++ src/git/NewFile.java
@@ -3,7 +3,11 @@ package git;
 public class NewFile {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("두번째파일");
+		
+		//커밋 후 수정을 하면
+		// > file명과 같이 바뀐다.
 
 	}
 
